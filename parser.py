c2fb5c6809fad4b5cf0b18f81868dcca2bcd96cc
Dmitriy <dimazar2004@gmail.com>
2024-12-06 16:16:38 +0300

    начал работу автоматизации парсинга

--- parser.py
+++ parser.py
@@ -2,8 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL страницы
+def get_links(query):
+    url = f'https://www.google.com/search?q={query}'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    links = []
+    for item in soup.find_all('h3'):
+        parent = item.find_parent('a')
+        if parent and 'href' in parent.attrs:
+            url = parent['href']
+            border = url.find('&sa')
+            # print(url[7:border])
+            links.append(url[7:border])
+
+    return links[:5]  # Возвращаем первые 5 ссылок
+
+user_query = "туристические места Архангельская область"
+urls = get_links(user_query)
+for url in urls:
+    print(url)
+
+# "https://www.tourister.ru/responses/id_43115"
 urls = ["https://travel.yandex.ru/journal/arhangelskaya-oblast/",
-        "https://www.kp.ru/russia/arhangelsk/dostoprimechatelnosti/?ysclid=m4cnvuyibd964033673"]
+        "https://www.kp.ru/russia/arhangelsk/dostoprimechatelnosti/?ysclid=m4cnvuyibd964033673",]
     
 def parse_from_web(url):
     response = requests.get(url)
@@ -20,6 +43,13 @@ def parse_from_web(url):
     else:
         return ''
 
+def get_urls_places():
+    pass
+
+def get_urls_review():
+    pass
+
 result_string = ''
 for url in urls:
     result_string += parse_from_web(url)
+print(result_string)
